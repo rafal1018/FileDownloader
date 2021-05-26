cdcd7e059cc99eb820a5cb1d17035a32d5e336d9
rafal1018 <rafaljaneczek@o2.pl>
2021-05-26 22:26:49 +0200

    initial commit

---
+++ src/test/java/pl/fileDownloader/FileDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fileDownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
